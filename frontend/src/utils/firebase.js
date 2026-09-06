3720846ab0f2150e08235753da0d36545bcf4585
PrasadDnyandeoGadekar <prasadgadekar461@gmail.com>
2026-09-06 10:46:43 +0000

    Added real Firebase configuration keys

---
+++ frontend/src/utils/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCkD0B5yqmhM-oxxDPUSc3h-RFGaeUVDuQ",
+  authDomain: "stemspark-ai.firebaseapp.com",
+  projectId: "stemspark-ai",
+  storageBucket: "stemspark-ai.firebasestorage.app",
+  messagingSenderId: "437914620679",
+  appId: "1:437914620679:web:7150744532b66044712ba2"
+};
